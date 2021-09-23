5f6b1feb50d23b20e5aff03eb8a119a326fe9a49
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-09-23 21:07:54 +0200

    basic config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  let containerStyle = {
+    backgroundColor: window.REACT_APP_NAVBAR_COLOR,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        ...containerStyle,
+        padding: 10,
+        height: 200,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      environment: <b>{window.REACT_APP_ENVIRONMENT}</b>
     </div>
   );
 }
